ba14d508457cb4631ce705d51bbd0415ccb5c8a3
shayanfpg9 <shayanfpg9@duck.com>
2023-07-11 15:54:09 +0330

    fix for long contents

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,6 +1,6 @@
 export default function Body(props) {
   return (
-    <main className="absolute w-[90%] h-[calc(100vh-(100vh/6+1.5rem+5rem))] mt-[calc(100vh/6+0.5rem)] left-1/2 -translate-x-1/2 bg-rose-200 bg-opacity-50 rounded-3xl -z-20 pt-8 pb-4 px-4">
+    <main className="absolute w-[90%] min-h-[calc(100vh-(100vh/6+1.5rem+5rem))] mt-[calc(100vh/6+0.5rem)] left-1/2 -translate-x-1/2 bg-rose-200 bg-opacity-50 rounded-3xl -z-20 pt-8 pb-4 px-4">
       {props.children}
     </main>
   );
