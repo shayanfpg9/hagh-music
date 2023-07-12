e277d419fe40a2c9888182fa27b27cd491e3b3b4
shayanfpg9 <shayanfpg9@duck.com>
2023-07-12 16:51:30 +0330

    add version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import { version } from "../../assets/config/config.json";
 
 export default function Footer() {
   return (
     <footer className="w-[95%] max-h-[8rem] fixed bottom-4 bg-rose-100 left-1/2 -translate-x-1/2 p-4 rounded-3xl text-center text-rose-900 font-medium -z-10 sm:text-xl lg:text-2xl shadow-lg shadow-rose-300">
-      copyright &#169; {new Date().getFullYear()}
+      ({version}) copyright &#169; {new Date().getFullYear()}
       <br />
       Designed by{" "}
       <Link
