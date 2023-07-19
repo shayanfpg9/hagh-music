a14e563968282ace6aa9f3216ac2cbe90aa3a85a
shayanfpg9 <shayanfpg9@duck.com>
2023-07-19 18:37:23 +0330

    change config + add bable eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,39 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    babelOptions: {
+      parserOpts: {
+        plugins: ["jsx"],
+      },
+    },
+    requireConfigFile: false,
+  },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  parser: "@babel/eslint-parser",
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/prop-types": [
+      1,
+      {
+        ignore: ["className", "children", "params"],
+      },
+    ],
+
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+
+    "no-unused-vars": "warn",
+
+    "react/jsx-uses-vars": "warn",
   },
-}
+};
