799a78210710d7f47dd6a3b7022b51ee62b5c6df
shayanfpg9 <shayanfpg9@duck.com>
2023-07-19 15:44:25 +0330

    dynamic team

--- src/components/Pages/Team.jsx
+++ src/components/Pages/Team.jsx
@@ -1,30 +1,21 @@
+import team from "../../assets/config/team.json";
+
 export default function Team() {
   return (
     <section className="h-full text-rose-700 flex justify-start flex-wrap items-start text-base sm:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl pr-4">
-      <div className="w-1/3 min-w-[20rem] mb-4">
-        <h2 className="text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl text-rose-900 mb-4">خوانندگان:</h2>
-        <ul>
-          <li>Itahaer</li>
-          <li>Mojrem</li>
-          <li>11AZG</li>
-          <li>Arya G</li>
-        </ul>
-      </div>
-
-      <div className="w-1/3 min-w-[20rem] mb-4">
-        <h2 className="text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl text-rose-900 mb-4">تهیه کنندگان:</h2>
-        <ul>
-          <li>11AZG</li>
-          <li>Arya G</li>
-        </ul>
-      </div>
-
-      <div className="w-1/3 min-w-[20rem]">
-        <h2 className="text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl text-rose-900 mb-4">هنرمندان:</h2>
-        <ul>
-          <li>Shayan</li>
-        </ul>
-      </div>
+      {...team.map((group, i) => (
+        <div
+          key={`group-team-` + i}
+          className={"w-1/3 min-w-[20rem] " + (i < team.length - 1 && "mb-6")}
+        >
+          <h2 className="text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl text-rose-900 mb-2">
+            {group.name}
+          </h2>
+          <ul>
+            {...group.items.map((user) => <li key={`user-` + user}>{user}</li>)}
+          </ul>
+        </div>
+      ))}
     </section>
   );
 }
