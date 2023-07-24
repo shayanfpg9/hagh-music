a4a31d77f29c4ff9172b18c42d80aae8cf4ff854
shayanfpg9 <shayanfpg9@duck.com>
2023-07-24 16:07:00 +0330

    static + chango color + pass config

--- upload/server.js
+++ upload/server.js
@@ -4,16 +4,22 @@ const fileUpload = require("express-fileupload");
 const response = require("./response");
 const path = require("path");
 const PORT = 4001;
+const config = require("../src/assets/config/config.json");
 
 // Listen:
 app.listen(PORT, () => {
-  console.log("\x1b[32m", "\n-> Start uploading...\n");
+  console.log("\x1b[32m", "\n-> Start uploading...\n","\x1b[0m");
 });
 
 // Run:
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./pages"));
 app.use(express.static(path.join(__dirname, "/styles")));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(
+  "/fonts",
+  express.static(path.join(__dirname, "../node_modules/vazirmatn/fonts"))
+);
 
 app.use(
   fileUpload({
@@ -27,24 +33,28 @@ app.use(
 app.get("/", (req, res) => {
   res.render("main", {
     page: "index",
+    config: config,
   });
 });
 
 app.get("/music", (req, res) => {
   res.render("main", {
     page: "music",
+    config: config,
   });
 });
 
 app.get("/album", (req, res) => {
   res.render("main", {
     page: "album",
+    config: config,
   });
 });
 
 app.get("/team", (req, res) => {
   res.render("main", {
     page: "team",
+    config: config,
   });
 });
 
