c96d7479ce94d18c5f264fcedf57068f624275a9
shayanfpg9 <shayanfpg9@duck.com>
2023-07-25 17:36:02 +0330

    static scripts + pass data

--- upload/server.js
+++ upload/server.js
@@ -5,16 +5,18 @@ const response = require("./response");
 const path = require("path");
 const PORT = 4001;
 const config = require("../src/assets/config/config.json");
+const albums = require("../src/assets/config/albums.json");
 
 // Listen:
 app.listen(PORT, () => {
-  console.log("\x1b[32m", "\n-> Start uploading...\n","\x1b[0m");
+  console.log("\x1b[32m", "\n-> Start uploading...\n", "\x1b[0m");
 });
 
 // Run:
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./pages"));
 app.use(express.static(path.join(__dirname, "/styles")));
+app.use(express.static(path.join(__dirname, "/scripts")));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(
   "/fonts",
@@ -41,6 +43,9 @@ app.get("/music", (req, res) => {
   res.render("main", {
     page: "music",
     config: config,
+    data: {
+      albums: albums.map((album) => album.name),
+    },
   });
 });
 
